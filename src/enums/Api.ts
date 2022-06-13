5767d5831db419223ffacbf99769c02462eb6137
Nelo Crypto <nelo.crypto@protonmail.com>
2022-06-14 00:01:31 +0100

    Change to Heroku

--- src/enums/Api.ts
+++ src/enums/Api.ts
@@ -1,5 +1,6 @@
 const API = {
-    GRAPHQL_ENDPOINT: 'http://localhost:3002',
+    //GRAPHQL_ENDPOINT: 'http://localhost:3002',
+    GRAPHQL_ENDPOINT: 'http://feed-watcher-graphql.herokuapp.com:3002',
 }
 
 export default API
