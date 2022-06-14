12c49f2d98d5295ac99fa2fc32789043227abfc1
Nelo Crypto <nelo.crypto@protonmail.com>
2022-06-14 02:12:56 +0100

    Improve Homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,18 @@ export default function Index() {
     return (
         <Layout pageTitle={'Home'}>
             <Row>
-                <Col sm="12">Home</Col>
+                <Col sm="12">
+                    <h1>Home</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col sm="12">
+                    <p>Welcome to Feed Viewer, a frontend client built with ReactJS and NextJS.</p>
+                    <p>Feeds:</p>
+                    <ul>
+                        <li><a href="/objects/1">Best standard prints</a></li>
+                    </ul>
+                </Col>
             </Row>
         </Layout>
     )
