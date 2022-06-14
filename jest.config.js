36f8bbb4fdfa049245153374c760290f2c92745d
Nelo Crypto <nelo.crypto@protonmail.com>
2022-06-14 04:48:40 +0100

    tsconfig for Jest

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,12 @@ module.exports = {
         '^.+\\.(ts|tsx)?$': 'ts-jest',
         '^.+\\.(js|jsx)$': 'babel-jest',
     },
-    testEnvironment: "jsdom"
+    testEnvironment: "jsdom",
+    verbose: true,
+    // Hack to avoid NextJS changing jsx to "preserve" with "yarn run dev"
+    "globals": {
+        "ts-jest": {
+            "tsconfig": "./tsconfig.test.json"
+        }
+    },
 }
